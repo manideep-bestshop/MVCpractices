d46cac5c48c168919963a3bfe9ff8ff1082bb6e1
Admin <Admin@DESKTOP-7T0EOMO>
2024-02-21 21:29:46 +0530

    Add project files.

---
+++ SampleMVCprogram/Controllers/HelloWorld.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SampleMVCprogram.Controllers
+{
+    public class HelloWorld : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Manideep";
+            return View();
+        }
+    }
+}
