e21b925e20266d16ee05b60a4517f51fd1783356
Admin <Admin@DESKTOP-7T0EOMO>
2024-03-04 22:04:53 +0530

    Implemented code first approach

---
+++ CodeFisrtApproach.Web/Controllers/StudentsController.cs
@@ -0,0 +1,79 @@
+﻿using CodeFirstApproach.Web.Data;
+using CodeFirstApproach.Web.Models;
+using CodeFirstApproach.Web.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstApproach.Web.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public readonly ApplicationDbContext DbContext;
+        public StudentsController(ApplicationDbContext dbContext)
+        {
+            this.DbContext = dbContext;
+        }
+
+
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddStudentViewModel viewModel)
+        {
+            var student = new Student
+            {
+                Name = viewModel.Name,
+                Email = viewModel.Email,
+                Phone = viewModel.Phone,
+                Subscribed = viewModel.Subscribed
+            };
+            await DbContext.Students.AddAsync(student);
+            await DbContext.SaveChangesAsync();
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            var students = await DbContext.Students.ToListAsync();
+            return View(students);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var student = await DbContext.Students.FindAsync(id);
+            return View(student);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Student viewModel)
+        {
+            var student = await DbContext.Students.FindAsync(viewModel.Id);
+            if (student is not null)
+            {
+                student.Name = viewModel.Name;
+                student.Email = viewModel.Email;
+                student.Phone = viewModel.Phone;
+                student.Subscribed = viewModel.Subscribed;
+
+                await DbContext.SaveChangesAsync();
+            }
+            return RedirectToAction("List", "Students");
+        }
+        public async Task<IActionResult> Delete(Student viewModel)
+        {
+            var student = await DbContext.Students.FindAsync(viewModel.Id);
+            if (student is not null)
+            {
+                DbContext.Students.Remove(student);
+                await DbContext.SaveChangesAsync();
+            }
+            return RedirectToAction("List", "Students");
+
+        }
+    }
+}
---
+++ CodeFisrtApproach.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CodeFirstApproach.Web.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstApproach.Web.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
